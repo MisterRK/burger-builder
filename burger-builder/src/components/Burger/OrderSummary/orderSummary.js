8cce4030d1abc38886d56271b1bb9777a0bb9154
MisterRK <robert.keller22@gmail.com>
2020-07-22 16:58:26 -0400

    Slight change to file structure. Improved performance by checking if modal needs to update

--- burger-builder/src/components/Burger/OrderSummary/orderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/orderSummary.js
@@ -1,29 +1,32 @@
-import React from "react";
-import Aux from "../../hoc/Aux";
-import Button from '../../UI/Button/Button'
+import React, { Component } from "react";
+import Aux from "../../hoc/Aux/Aux";
+import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
   const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
+
     return (
       <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>
-        :
+        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
         {props.ingredients[igKey]}
       </li>
     );
   });
-  // <li>Salad: 1 </li>
   return (
     <Aux>
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: ${props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancel}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinue}>SUBMIT</Button>
+      <Button btnType="Danger" clicked={props.purchaseCancel}>
+        CANCEL
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinue}>
+        SUBMIT
+      </Button>
     </Aux>
   );
 };
