8dee813ec828b49dab9996f0917baff7dc68f114
MisterRK <robert.keller22@gmail.com>
2020-08-01 23:46:01 -0400

    Outputting orders dynamically from DB.

--- burger-builder/src/components/Order/Order.js
+++ burger-builder/src/components/Order/Order.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import classes from './Order.module.css'
 
-const order = (props) => (
-   <div className={classes.Order}>
-      <p>Ingredients: Salad (1)</p>
-      <p>Price: <strong>$$ 5.45</strong></p>
-   </div>
-);
+const order = (props) => {
+   console.log(props)
+   const ingredients = []
+   for (let ingredientName in props.ingredients){
+      ingredients.push(
+         {
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]}
+      );
+   }
+   const ingredientOutput = ingredients.map(ing => {
+      return <span className={classes.Ingredients} key={ing.name}>{ing.name} ({ing.amount}) </span>
+   });
+
+   return (
+      <div className={classes.Order}>
+         <p>Ingredients: {ingredientOutput}</p>
+         <p>Price: <strong>$$ {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+      </div>
+   )
+};
 
 export default order;
--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react'
 import Order from '../../components/Order/Order'
+import axios from '../../axios-orders'
+import withErrorHandler from '../../components/hoc/withErrorHandler/WithErrorHandler'
 
 class Orders extends Component {
+   state = {
+      orders: [],
+      loading: true
+   }
+
+   componentDidMount(){
+      axios.get('/orders.json')
+      .then(res => {
+         let fetchedOrders = []
+         for(let key in res.data){
+            fetchedOrders.push({
+               ...res.data[key],
+               id: key})
+         }
+         this.setState({orders: fetchedOrders, loading: false})
+      })
+      .catch(err => {
+         this.setState({loading:false})
+      })
+   }
    render () {
+      console.log(this.state)
       return(
          <div>
-            <Order />
-            <Order />
+            {this.state.orders.map(order => (
+               <Order 
+                  key={order.id} 
+                  ingredients={order.ingredients}
+                  price={order.price}
+               />
+            ))}
          </div>
       );
    }
 }
 
-export default Orders
+export default withErrorHandler(Orders, axios);
