139e768f4dde1fbac360b7de4e709aa1ed3691c7
MisterRK <robert.keller22@gmail.com>
2020-08-24 19:21:26 -0400

    installed Enzyme packages for unit testing

--- burger-builder/src/components/hoc/withErrorHandler/WithErrorHandler.js
+++ burger-builder/src/components/hoc/withErrorHandler/WithErrorHandler.js
@@ -9,7 +9,7 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null,
     };
     // set the interceptors to work globally
-    componentWillMount() {
+    componentDidMount() {
       this.reqInterceptor = axios.interceptors.request.use((req) => {
         this.setState({ error: null });
         return req;
